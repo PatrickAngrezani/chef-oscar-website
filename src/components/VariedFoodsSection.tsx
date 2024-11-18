0bf0ced3cb059dd480a2d8757022ae9d35280ae7
PatrickAngrezani <pangrezani@gmail.com>
2024-11-17 23:01:28 -0300

    update varied foods with elements

--- src/components/VariedFoodsSection.tsx
+++ src/components/VariedFoodsSection.tsx
@@ -1,17 +1,66 @@
-import { Box, Typography } from "@mui/material";
+import { Box, List, ListItem, Typography } from "@mui/material";
 import React from "react";
+
+import oscarLogo from "../assets/background/chef-oscar-logo.jpg";
+import calzoneImg from "../assets/menu-online/varied-foods/calzone.jpg";
+import farfalleImg from "../assets/menu-online/varied-foods/farfalle-macarrao.jpg";
+import iscaPeixeImg from "../assets/menu-online/varied-foods/isca-peixe.jpg";
+import xpizzaImg from "../assets/menu-online/varied-foods/xis-pizza.jpeg";
+import pratoFit from "../assets/menu-online/varied-foods/fit.png";
+import sanduiche from "../assets/menu-online/varied-foods/sanduiche-com-massa.jpg";
+import sanduiche2 from "../assets/menu-online/varied-foods/sanduiche-2.jpg";
+
+const VariedFoods = [
+  {
+    name: "Calzone",
+    description: "Peça nos mesmos sabores conforme o Cardápio de Pizza",
+    image: calzoneImg,
+  },
+  {
+    name: "Farfalle",
+    description:
+      "Macarrão de trigo com almondegas assadas de filé de Frango em Molho de tomate e salada na tigela.",
+    image: farfalleImg,
+  },
+  {
+    name: "Iscas de Peixe",
+    description: "Ao Molho Tártaro",
+    image: iscaPeixeImg,
+  },
+  {
+    name: "Prato Fit",
+    description: "Ingredientes",
+    image: pratoFit,
+  },
+  {
+    name: "Sanduiche",
+    description: "Ingredientes",
+    image: sanduiche,
+  },
+  {
+    name: "Sanduiche 2",
+    description: "Ingredientes",
+    image: sanduiche2,
+  },
+  {
+    name: "XPizza",
+    description: "Peça nos mesmos sabores conforme o Cardápio de Pizza",
+    image: xpizzaImg,
+  },
+];
 
 const VariedFoodsSection = () => {
   return (
     <Box
       sx={{
-        minHeight: "100%",
-        display: "flex",
-        justifyContent: "center",
-        allignItens: "center",
-        background: "linear-gradient(135deg, #FF4500, #FFD700, #FF6347)",
-        maxWidth: "100%",
+        backgroundAttachment: "fixed",
+        backgroundImage: `url(${oscarLogo})`,
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
         position: "relative",
+        padding: "20px",
+        minHeight: "100vh",
       }}
     >
       <Box
@@ -39,6 +88,67 @@ const VariedFoodsSection = () => {
         >
           Cardápio Variado{" "}
         </Typography>
+
+        <List>
+          {VariedFoods.map((food, index) => (
+            <ListItem
+              key={index}
+              sx={{
+                paddingLeft: 0,
+                display: "flex",
+                alignItems: "center",
+                marginBottom: 1,
+              }}
+            >
+              <Box
+                component="img"
+                src={food.image}
+                alt={food.name}
+                sx={{
+                  width: "80px",
+                  height: "80px",
+                  objectFit: "cover",
+                  borderRadius: "8px",
+                  marginRight: "20px",
+                  flexShrink: 0,
+                }}
+              />
+              <Box sx={{ display: "flex", flexDirection: "column" }}>
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <Typography
+                    sx={{
+                      color: "#4B2E2A",
+                      fontWeight: "bold",
+                      fontFamily: "Tangerine",
+                      fontSize: "1.75rem",
+                    }}
+                  >
+                    {food.name}:
+                  </Typography>
+                  <Box sx={{}}>
+                    <Typography
+                      component="span"
+                      sx={{
+                        color: "black",
+                        fontFamily: "Tangerine",
+                        marginLeft: 1,
+                        fontSize: "1.00rem",
+                        maxHeight: "6rem",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        display: "-webkit-box",
+                        WebkitBoxOrient: "vertical",
+                        WebkitLineClamp: 3,
+                      }}
+                    >
+                      {food.description}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+            </ListItem>
+          ))}
+        </List>
       </Box>
     </Box>
   );
